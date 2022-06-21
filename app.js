db69ddac2a4e59d72ac76238d7621a0040134c41
вашÐdenislebedev2015 <d3niz.lebedev@yandex.by>
2022-06-21 22:39:46 +0000

    ваше соfirst

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var http = require('http');
-var requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+const express = require('express');
+let app = express();
 
-var server = http.createServer(requestListener);
-server.listen(3000, function() { console.log("Listening on port 3000")});
-// change app
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
+
+app.listen(3000, function() {
+	console.log('running');
+});
